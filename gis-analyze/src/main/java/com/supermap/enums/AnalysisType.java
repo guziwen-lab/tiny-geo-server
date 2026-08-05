130c671fdda31f65b2ee893fd33e5502afd371e8
gzw <guziwen@outlook.com>
2026-08-05 14:55:16 +0800

    feat(gis-dataset): 相交面积拆分优化sql

--- gis-analyze/src/main/java/com/supermap/enums/AnalysisType.java
+++ gis-analyze/src/main/java/com/supermap/enums/AnalysisType.java
@@ -11,11 +11,11 @@ import lombok.Getter;
 public enum AnalysisType {
 
     OVERLAY("图层叠加"),
-    INTERSECT_SPLIT("相交面积拆分"),
     BUFFER("缓冲区分析"),
     DISSOLVE("消减"),
     SPATIAL_JOIN("空间连接"),
 
+    INTERSECT_SPLIT("相交面积拆分"),
     /**
      * 还可以修改srid
      */
