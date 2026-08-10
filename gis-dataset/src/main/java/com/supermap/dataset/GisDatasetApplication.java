22941f8783280227b1af6d64688e30f47064a711
gzw <guziwen@outlook.com>
2026-08-10 18:10:11 +0800

    refactor: 修改包名

--- gis-dataset/src/main/java/com/supermap/dataset/GisDatasetApplication.java
+++ gis-dataset/src/main/java/com/supermap/dataset/GisDatasetApplication.java
@@ -3,7 +3,7 @@ package com.supermap.dataset;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.supermap")
 public class GisDatasetApplication {
 
     public static void main(String[] args) {
--- gis-platform/src/main/java/com/supermap/platform/GisPlatformApplication.java
+++ gis-platform/src/main/java/com/supermap/platform/GisPlatformApplication.java
@@ -3,7 +3,7 @@ package com.supermap.platform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.supermap")
 public class GisPlatformApplication {
 
     public static void main(String[] args) {
